167d3cbf428b69dd45c3eb1a945acce273e17833
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-06-12 12:17:30 +0200

    Sort plugins constants alphabetically
    (cherry picked from commit 7cd5517eb8748936fa39ae608e9765c25d8b0f66)

--- buildSrc/src/main/kotlin/BuildPlugins.kt
+++ buildSrc/src/main/kotlin/BuildPlugins.kt
@@ -24,7 +24,7 @@ object BuildPlugins {
     const val DOKKA = "plugins.dokka"
     const val GIT_HOOKS = "plugins.git-hooks"
     const val KTLINT = "plugins.ktlint"
-    const val SPOTLESS = "plugins.spotless"
     const val SONARQUBE = "plugins.sonarqube"
+    const val SPOTLESS = "plugins.spotless"
     const val UPDATE_DEPENDENCIES = "plugins.update-dependencies"
 }
