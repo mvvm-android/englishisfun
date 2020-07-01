41be7853d1c9351572ff57621c7b66f02a5085bc
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-07-01 09:57:22 +0200

    Remove unnecessary testing code on FragmentExtensionsTest.

--- base-android/src/test/kotlin/com/jpaya/base/ui/extensions/FragmentExtensionsTest.kt
+++ base-android/src/test/kotlin/com/jpaya/base/ui/extensions/FragmentExtensionsTest.kt
@@ -23,73 +23,57 @@ import androidx.lifecycle.ViewModelProvider
 import com.jpaya.libraries.testutils.TestFragment
 import com.jpaya.libraries.testutils.robolectric.TestRobolectric
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers
+import org.hamcrest.Matchers.instanceOf
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class FragmentExtensionsTest : TestRobolectric() {
 
-    private open class TestViewModel(val state: Lifecycle.State? = null) : ViewModel()
-
     @Test
-    fun providedViewModel_ShouldObtainWithStateSaved() {
-        val fragmentScenario = launchFragmentInContainer<TestFragment>()
-        val expectedState = Lifecycle.State.INITIALIZED
-
-        fragmentScenario.onFragment {
+    fun providedViewModelByNothing_ShouldObtainWithStateSaved() {
+        val scenario = launchFragmentInContainer<TestFragment>()
+        scenario.onFragment {
+            val expectedState = Lifecycle.State.INITIALIZED
             val createdViewModel = it.viewModel {
-                TestViewModel(
-                    expectedState
-                )
+                TestViewModel(expectedState)
             }
-            assertThat(createdViewModel, Matchers.instanceOf(TestViewModel::class.java))
+            assertThat(createdViewModel, instanceOf(TestViewModel::class.java))
             assertEquals(expectedState, createdViewModel.state)
-
-            val providedViewModel = ViewModelProvider(it).get(TestViewModel::class.java)
-            assertThat(providedViewModel, Matchers.instanceOf(TestViewModel::class.java))
-            assertEquals(expectedState, providedViewModel.state)
         }
     }
 
     @Test
     fun providedViewModelByIdentifier_ShouldObtainWithStateSaved() {
-        val fragmentScenario = launchFragmentInContainer<TestFragment>()
-        val expectedState = Lifecycle.State.INITIALIZED
-        val identifierViewModel = "TestViewModel"
-
-        fragmentScenario.onFragment {
-            val createdViewModel =
-                it.viewModel(identifierViewModel) {
-                    TestViewModel(
-                        expectedState
-                    )
-                }
-            assertThat(createdViewModel, Matchers.instanceOf(TestViewModel::class.java))
+        val scenario = launchFragmentInContainer<TestFragment>()
+        scenario.onFragment {
+            val identifier = "TestViewModel"
+            val expectedState = Lifecycle.State.INITIALIZED
+            val createdViewModel = it.viewModel(identifier) {
+                TestViewModel(expectedState)
+            }
+            assertThat(createdViewModel, instanceOf(TestViewModel::class.java))
             assertEquals(expectedState, createdViewModel.state)
-
-            val providedViewModel = ViewModelProvider(it).get(identifierViewModel, TestViewModel::class.java)
-            assertThat(providedViewModel, Matchers.instanceOf(TestViewModel::class.java))
-            assertEquals(expectedState, providedViewModel.state)
         }
     }
 
-    @Test(expected = RuntimeException::class)
-    fun providedViewModelWithWrongIdentifier_ShouldNotObtain() {
-        val fragmentScenario = launchFragmentInContainer<TestFragment>()
-        val identifierViewModel = "TestViewModel"
-
-        fragmentScenario.onFragment {
-            it.viewModel(identifierViewModel) { TestViewModel() }
-            ViewModelProvider(it).get("Wrong Identifier", TestViewModel::class.java)
+    @Test
+    fun providedViewModelByFactory_ShouldObtainWithStateSaved() {
+        val scenario = launchFragmentInContainer<TestFragment>()
+        scenario.onFragment {
+            val expectedState = Lifecycle.State.INITIALIZED
+            val createdViewModel = it.viewModel {
+                MyViewModelFactory(expectedState).create(TestViewModel::class.java)
+            }
+            assertThat(createdViewModel, instanceOf(TestViewModel::class.java))
+            assertEquals(expectedState, createdViewModel.state)
         }
     }
 
-    @Test(expected = RuntimeException::class)
-    fun notProvidedViewModel_ShouldNotObtain() {
-        val fragmentScenario = launchFragmentInContainer<TestFragment>()
+    private class TestViewModel(val state: Lifecycle.State? = null) : ViewModel()
 
-        fragmentScenario.onFragment {
-            ViewModelProvider(it).get(TestViewModel::class.java)
+    private class MyViewModelFactory(val state: Lifecycle.State?) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return modelClass.getConstructor(Lifecycle.State::class.java).newInstance(state)
         }
     }
 }
