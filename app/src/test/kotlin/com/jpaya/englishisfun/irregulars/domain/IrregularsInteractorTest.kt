110cf8d18bec084054d91a268fb536332571dcbd
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-07-15 10:44:41 +0200

    Testing IrregularsInteractor

---
+++ app/src/test/kotlin/com/jpaya/englishisfun/irregulars/domain/IrregularsInteractorTest.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2020 Jose Maria Payá Castillo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jpaya.englishisfun.irregulars.domain
+
+import com.jpaya.englishisfun.irregulars.data.network.NetworkDataSource
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class IrregularsInteractorTest {
+
+    companion object {
+        private val MOCK_IRREGULARS_ITEMS = listOf(
+            Irregulars(
+                id = 1,
+                base = "Base 1",
+                simple = "Simple 1",
+                participle = "Participle 1",
+                definitions = "Definitions 1"
+            ),
+            Irregulars(
+                id = 2,
+                base = "Base 2",
+                simple = "Simple 2",
+                participle = "Participle 2",
+                definitions = "Definitions 2"
+            )
+        )
+    }
+
+    private lateinit var dataSource: NetworkDataSource
+    private lateinit var interactor: IrregularsInteractor
+
+    @Before
+    fun setUp() {
+        dataSource = mock()
+        interactor = IrregularsInteractor(dataSource)
+    }
+
+    @Test
+    fun `Check getIrregularsItems works properly`() = runBlockingTest {
+        whenever(dataSource.getIrregularsItems()).doReturn(MOCK_IRREGULARS_ITEMS)
+
+        assertEquals(MOCK_IRREGULARS_ITEMS, interactor.getIrregularsItems())
+    }
+}
