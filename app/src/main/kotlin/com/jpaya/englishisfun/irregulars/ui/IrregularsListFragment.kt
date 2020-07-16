271aa6b3b9054d56d71a91e250b43212306af01d
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-07-16 12:47:23 +0200

    Remove unused import

--- app/src/main/kotlin/com/jpaya/englishisfun/irregulars/ui/IrregularsListFragment.kt
+++ app/src/main/kotlin/com/jpaya/englishisfun/irregulars/ui/IrregularsListFragment.kt
@@ -19,7 +19,6 @@ package com.jpaya.englishisfun.irregulars.ui
 import android.os.Bundle
 import android.transition.TransitionManager
 import android.view.View
-import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
