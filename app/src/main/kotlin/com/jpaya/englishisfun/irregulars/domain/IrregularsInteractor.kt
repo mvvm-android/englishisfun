13823edd6f100b9f8a663abd90cc4afa4ef8c6a3
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-07-17 08:46:40 +0200

    Refactoring

--- app/src/main/kotlin/com/jpaya/englishisfun/irregulars/domain/IrregularsInteractor.kt
+++ app/src/main/kotlin/com/jpaya/englishisfun/irregulars/domain/IrregularsInteractor.kt
@@ -27,7 +27,7 @@ class IrregularsInteractor @Inject constructor(
 
     suspend fun getIrregularsItems(): List<Irregulars> {
         val result = network.getIrregularsItems()
-        database.save(result[0])
+        database.save(result)
         return result
     }
 }
