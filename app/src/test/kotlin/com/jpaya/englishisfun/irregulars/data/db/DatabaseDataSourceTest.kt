0c6b99045d9c952f2eebf21b3b43241987c3b631
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-08-03 12:43:53 +0200

    Uncomment DatabaseDataSourceTest

--- app/src/test/kotlin/com/jpaya/englishisfun/irregulars/data/db/DatabaseDataSourceTest.kt
+++ app/src/test/kotlin/com/jpaya/englishisfun/irregulars/data/db/DatabaseDataSourceTest.kt
@@ -16,74 +16,74 @@
 
 package com.jpaya.englishisfun.irregulars.data.db
 
+import androidx.room.Room
+import com.jpaya.englishisfun.irregulars.domain.Irregulars
 import com.jpaya.libraries.testutils.robolectric.TestRobolectric
-
-// import com.jpaya.englishisfun.irregulars.domain.Irregulars
-// import com.jpaya.libraries.testutils.robolectric.TestRobolectric
-// import kotlinx.coroutines.runBlocking
-// import org.junit.Assert.assertEquals
-// import org.junit.Before
-// import org.junit.Test
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
 
 class DatabaseDataSourceTest : TestRobolectric() {
 
-//    private lateinit var dataSource: DatabaseDataSource
-//
-//    private val item1 = Irregulars(
-//        id = 1,
-//        base = "Base",
-//        simple = "Simple",
-//        participle = "Participle",
-//        definitions = "Definitions"
-//    )
-//
-//    private val item2 = Irregulars(
-//        id = 2,
-//        base = "Base",
-//        simple = "Simple",
-//        participle = "Participle",
-//        definitions = "Definitions"
-//    )
-//
-//    private val item3 = Irregulars(
-//        id = 3,
-//        base = "Base",
-//        simple = "Simple",
-//        participle = "Participle",
-//        definitions = "Definitions"
-//    )
-//
-//    @Before
-//    fun setUp() {
-//        dataSource = DatabaseDataSource(context)
-//    }
-//
-//    @Test
-//    fun `Check save works properly`() = runBlocking {
-//        assertEquals(0, dataSource.count())
-//
-//        dataSource.save(item1)
-//        assertEquals(1, dataSource.count())
-//
-//        dataSource.save(listOf(item2, item3))
-//        assertEquals(3, dataSource.count())
-//
-//        // Save duplicated item
-//        dataSource.save(item2)
-//        assertEquals(3, dataSource.count())
-//
-//        val list = dataSource.all()
-//        assertEquals(3, list.size)
-//        assertEquals(listOf(item1, item2, item3), list)
-//
-//        // Delete unexisting element
-//        dataSource.delete(4)
-//        assertEquals(3, dataSource.count())
-//
-//        dataSource.delete(3)
-//        assertEquals(2, dataSource.count())
-//
-//        dataSource.delete()
-//        assertEquals(0, dataSource.count())
-//    }
+    private lateinit var dataSource: DatabaseDataSource
+
+    private val item1 = Irregulars(
+        id = 1,
+        base = "Base",
+        simple = "Simple",
+        participle = "Participle",
+        definitions = "Definitions"
+    )
+
+    private val item2 = Irregulars(
+        id = 2,
+        base = "Base",
+        simple = "Simple",
+        participle = "Participle",
+        definitions = "Definitions"
+    )
+
+    private val item3 = Irregulars(
+        id = 3,
+        base = "Base",
+        simple = "Simple",
+        participle = "Participle",
+        definitions = "Definitions"
+    )
+
+    @Before
+    fun setUp() {
+        val database = Room.inMemoryDatabaseBuilder(context, IrregularsDatabase::class.java).build()
+        dataSource = DatabaseDataSource(database.irregulars())
+    }
+
+    @Test
+    fun `Check irregulars works properly`() = runBlocking {
+        assertEquals(0, dataSource.count())
+
+        dataSource.save(item1)
+        assertEquals(1, dataSource.count())
+
+        dataSource.save(listOf(item2, item3))
+        assertEquals(3, dataSource.count())
+
+        // Save duplicated item
+        dataSource.save(item2)
+        assertEquals(3, dataSource.count())
+
+        val list = dataSource.all()
+        assertEquals(3, list.size)
+        assertEquals(listOf(item1, item2, item3), list)
+
+        // Delete unexisting element
+        dataSource.delete(4)
+        assertEquals(3, dataSource.count())
+
+        dataSource.delete(3)
+        assertEquals(2, dataSource.count())
+
+        dataSource.delete()
+        assertEquals(0, dataSource.count())
+    }
 }
