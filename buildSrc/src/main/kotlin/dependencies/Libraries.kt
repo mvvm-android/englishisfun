c7abcc407fa906ec630ed5e9e83323008982f77a
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-08-05 07:15:38 +0200

    Update coroutines libraries to 1.3.8

--- buildSrc/src/main/kotlin/dependencies/Libraries.kt
+++ buildSrc/src/main/kotlin/dependencies/Libraries.kt
@@ -34,7 +34,7 @@ object Libraries {
         const val COMPOSE = "0.1.0-dev14"
         const val CONSTRAINT_LAYOUT = "1.1.3"
         const val CORE_KTX = "1.3.0"
-        const val COROUTINES = "1.3.7"
+        const val COROUTINES = "1.3.8"
         const val FIREBASE_ANALYTICS = "17.4.4"
         const val FIREBASE_AUTH = "19.3.2"
         const val FIREBASE_CRASHLYTICS = "17.1.1"
