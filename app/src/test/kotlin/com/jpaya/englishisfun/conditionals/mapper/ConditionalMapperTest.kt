8820294d95c4bcf47c8807d832cb679392bbd4a2
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-09-01 15:39:28 +0200

    Review conditionals package

--- app/src/test/kotlin/com/jpaya/englishisfun/conditionals/mapper/ConditionalMapperTest.kt
+++ app/src/test/kotlin/com/jpaya/englishisfun/conditionals/mapper/ConditionalMapperTest.kt
@@ -14,15 +14,16 @@
  * limitations under the License.
  */
 
-package com.jpaya.englishisfun.conditionals.data.db
+package com.jpaya.englishisfun.conditionals.mapper
 
+import com.jpaya.englishisfun.conditionals.data.db.ConditionalRoomItem
+import com.jpaya.englishisfun.conditionals.data.network.model.ConditionalNetworkItem
 import com.jpaya.englishisfun.conditionals.domain.Conditional
-import com.jpaya.englishisfun.conditionals.mapper.toDomain
-import com.jpaya.englishisfun.conditionals.mapper.toRoomItem
+import com.jpaya.englishisfun.conditionals.ui.model.ConditionalItem
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class DiskModelMappingTest {
+class ConditionalMapperTest {
 
     @Test
     fun `Check domain to room works properly`() {
@@ -68,5 +69,51 @@ class DiskModelMappingTest {
         )
 
         assertEquals(expectedResult, room.toDomain())
+    }
+
+    @Test
+    fun `Check network to domain works properly`() {
+        val network = ConditionalNetworkItem().apply {
+            id = 1
+            name = "Name"
+            condition = "Condition"
+            result = "Result"
+            uses = mutableListOf("Use 1")
+            examples = mutableListOf("Example 1")
+        }
+
+        val expectedResult = Conditional(
+            id = 1,
+            name = "Name",
+            condition = "Condition",
+            result = "Result",
+            uses = mutableListOf("Use 1"),
+            examples = mutableListOf("Example 1")
+        )
+
+        assertEquals(expectedResult, network.toDomain())
+    }
+
+    @Test
+    fun `Check domain to presentation works properly`() {
+        val domain = Conditional(
+            id = 1,
+            name = "Name",
+            condition = "Condition",
+            result = "Result",
+            uses = mutableListOf("Use 1"),
+            examples = mutableListOf("Example 1")
+        )
+
+        val expectedResult = ConditionalItem(
+            id = 1,
+            name = "Name",
+            condition = "Condition",
+            result = "Result",
+            uses = "Use 1",
+            examples = "Example 1"
+        )
+
+        assertEquals(expectedResult, domain.toPresentation())
     }
 }
