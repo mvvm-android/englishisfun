443ebb1790956a8b8a218a14a2a35d824027541e
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-09-03 09:26:42 +0200

    Testing StativesAdapter

--- app/src/test/kotlin/com/jpaya/englishisfun/statives/ui/adapter/StativesAdapterTest.kt
+++ app/src/test/kotlin/com/jpaya/englishisfun/statives/ui/adapter/StativesAdapterTest.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Jose Maria Payá Castillo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.jpaya.englishisfun.statives.ui.adapter
 
 import android.widget.FrameLayout
@@ -20,8 +36,8 @@ class StativesAdapterTest : TestRobolectric() {
 
     @Test
     fun `Check onCreateViewHolder works properly`() {
-        val candyViewHolder = adapter.onCreateViewHolder(FrameLayout(context), 0)
-        assertNotNull(candyViewHolder)
-        assertThat(candyViewHolder.binding, instanceOf(StativeListItemBinding::class.java))
+        val viewHolder = adapter.onCreateViewHolder(FrameLayout(context), 0)
+        assertNotNull(viewHolder)
+        assertThat(viewHolder.binding, instanceOf(StativeListItemBinding::class.java))
     }
 }
