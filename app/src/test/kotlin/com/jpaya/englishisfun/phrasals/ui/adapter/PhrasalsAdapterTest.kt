5534b56dc4358726923f5626da34f8bd59a2cf70
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-09-04 09:21:40 +0200

    Modify invalid imports

--- app/src/test/kotlin/com/jpaya/englishisfun/phrasals/ui/adapter/PhrasalsAdapterTest.kt
+++ app/src/test/kotlin/com/jpaya/englishisfun/phrasals/ui/adapter/PhrasalsAdapterTest.kt
@@ -17,9 +17,9 @@
 package com.jpaya.englishisfun.phrasals.ui.adapter
 
 import android.widget.FrameLayout
+import com.jpaya.base.testutils.TestRobolectric
 import com.jpaya.englishisfun.databinding.PhrasalsListItemBinding
 import com.jpaya.englishisfun.phrasals.ui.model.PhrasalItem
-import com.jpaya.libraries.testutils.robolectric.TestRobolectric
 import org.hamcrest.CoreMatchers
 import org.junit.Assert.*
 import org.junit.Before
