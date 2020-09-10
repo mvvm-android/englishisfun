20c888355190220ab9966fef59795066f8ca94be
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-09-10 08:41:03 +0200

    Testing ConditionalsListViewState class

---
+++ app/src/test/kotlin/com/jpaya/englishisfun/conditionals/ui/ConditionalsListViewStateTest.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2020 Jose Maria Payá Castillo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jpaya.englishisfun.conditionals.ui
+
+import com.jpaya.englishisfun.conditionals.ui.ConditionalsListViewState.Loading
+import com.jpaya.englishisfun.conditionals.ui.ConditionalsListViewState.ListReady
+import com.jpaya.englishisfun.conditionals.ui.ConditionalsListViewState.NetworkError
+import com.jpaya.englishisfun.conditionals.ui.model.ConditionalItem
+import org.junit.Assert.*
+import org.junit.Test
+
+class ConditionalsListViewStateTest {
+
+    @Test
+    fun `Check Loading state works properly`() {
+        val loading = Loading
+        assertTrue(loading.showLoading())
+        assertFalse(loading.showError())
+        assertFalse(loading.showList())
+        assertTrue(loading.list().isEmpty())
+    }
+
+    @Test
+    fun `Check ListReady state works properly`() {
+        val conditionals = listOf(
+            ConditionalItem(
+                id = 1,
+                name = "Name",
+                condition = "Condition",
+                result = "Result",
+                uses = "Uses",
+                examples = "Examples"
+            )
+        )
+        val loading = ListReady(conditionals)
+        assertFalse(loading.showLoading())
+        assertFalse(loading.showError())
+        assertTrue(loading.showList())
+        assertEquals(conditionals, loading.list())
+    }
+
+    @Test
+    fun `Check NetworkError state works properly`() {
+        val loading = NetworkError
+        assertFalse(loading.showLoading())
+        assertTrue(loading.showError())
+        assertFalse(loading.showList())
+        assertTrue(loading.list().isEmpty())
+    }
+}
