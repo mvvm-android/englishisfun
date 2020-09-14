f6cbe6ae210be05ae7a480826ccd583facfe2743
Jose Maria Payá Castillo <josepaya17@gmail.com>
2020-09-14 09:51:08 +0200

    Testing ThemeUtilsImpl class

---
+++ base-android/src/test/kotlin/com/jpaya/base/utils/ThemeUtilsImplTest.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 Jose Maria Payá Castillo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jpaya.base.utils
+
+import com.jpaya.base.extensions.isNightTime
+import com.jpaya.libraries.testutils.robolectric.TestRobolectric
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import java.util.Calendar
+
+class ThemeUtilsImplTest : TestRobolectric() {
+
+    private lateinit var themeUtils: ThemeUtilsImpl
+
+    @Before
+    fun setUp() {
+        themeUtils = ThemeUtilsImpl()
+    }
+
+    @Test
+    fun `Check setAppearance works properly`() {
+        assertTrue(themeUtils.isLightTheme())
+        assertFalse(themeUtils.isDarkTheme())
+
+        // Dark appearance
+        themeUtils.setAppearance("dark")
+        assertFalse(themeUtils.isLightTheme())
+        assertTrue(themeUtils.isDarkTheme())
+
+        // Light appearance
+        themeUtils.setAppearance("light")
+        assertTrue(themeUtils.isLightTheme())
+        assertFalse(themeUtils.isDarkTheme())
+
+        // Auto appearance
+        themeUtils.setAppearance("auto")
+        if (Calendar.getInstance().isNightTime()) {
+            assertFalse(themeUtils.isLightTheme())
+            assertTrue(themeUtils.isDarkTheme())
+        } else {
+            assertTrue(themeUtils.isLightTheme())
+            assertFalse(themeUtils.isDarkTheme())
+        }
+    }
+}
